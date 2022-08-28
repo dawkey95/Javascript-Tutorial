4e7eb7438275d3681aa3ad9c169364c15ee2d25e
dawkey95 <dawidkeyser95@gmail.com>
2022-08-29 09:42:55 +1200

    data file created and logic split

--- tutorial/09-array-methods.js
+++ tutorial/09-array-methods.js
@@ -1,6 +1,4 @@
 /*
-## Array Methods #9
-
 #### Create Students Array
 
 1. setup students array with 5 students
@@ -51,40 +49,5 @@
 2. assign to survey and log
 
 */
-
-
-// Create Students Array
-const students = [
-	{
-		id: 1,
-		name: "Dave",
-		score: 33,
-		favouriteSubject: "Math",
-	},
-	{
-		id: 2,
-		name: "Susan",
-		score: 64,
-		favouriteSubject: "Art",
-	},
-	{
-		id: 3,
-		name: "Mitch",
-		score: 40,
-		favouriteSubject: "Math",
-	},
-	{
-		id: 4,
-		name: "Anne",
-		score: 95,
-		favouriteSubject: "History",
-	},
-	{
-		id: 5,
-		name: "Mark",
-		score: 73,
-		favouriteSubject: "History",
-	}
-];
 
 console.log(students);
---
+++ tutorial/10-data.js
@@ -0,0 +1,33 @@
+// Create Students Array
+const students = [
+	{
+		id: 1,
+		name: "Dave",
+		score: 33,
+		favouriteSubject: "Math",
+	},
+	{
+		id: 2,
+		name: "Susan",
+		score: 64,
+		favouriteSubject: "Art",
+	},
+	{
+		id: 3,
+		name: "Mitch",
+		score: 40,
+		favouriteSubject: "Math",
+	},
+	{
+		id: 4,
+		name: "Anne",
+		score: 95,
+		favouriteSubject: "History",
+	},
+	{
+		id: 5,
+		name: "Mark",
+		score: 73,
+		favouriteSubject: "History",
+	},
+]
