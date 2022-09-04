0b509c641e7f612ab46dd43b9943e078e75cd4a5
dawkey95 <dawidkeyser95@gmail.com>
2022-09-05 10:20:21 +1200

    Review Prouject Completed

--- Projects/03-reviews/app.js
+++ Projects/03-reviews/app.js
@@ -37,3 +37,66 @@ const reviews = [
       "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ",
   },
 ];
+
+
+// select items
+const img = document.getElementById('person-img');
+const author = document.getElementById('author');
+const job = document.getElementById('job');
+const textInfo = document.getElementById('info');
+
+const prevBtn = document.querySelector('.prev-btn');
+const nextBtn = document.querySelector('.next-btn');
+const randomBtn = document.querySelector('.random-btn');
+
+// set starting item
+let currentItem = 0;
+
+// load initial item
+window.addEventListener('DOMContentLoaded', function() {
+  showPerson(currentItem);
+})
+
+// show person based on item
+function showPerson(person) {
+const item = reviews[person]
+img.src = item.img
+author.textContent = item.name
+job.textContent = item.job
+textInfo.textContent = item.text
+}
+
+
+// shows a next review
+nextBtn.addEventListener('click', function() {
+  currentItem++;
+  if(currentItem > reviews.length - 1) {
+    currentItem = 0;
+  }
+  showPerson(currentItem);
+})
+
+
+// shows a previous review
+prevBtn.addEventListener('click', function() {
+  currentItem--;
+  if(currentItem < 0) {
+    currentItem = reviews.length - 1;
+  }
+		showPerson(currentItem)
+})
+
+
+/*
+ * shows a random review
+ * while randomNum is the same as currentItem we will re-roll a random number
+ * sets new random number as the currentItem index
+ */
+randomBtn.addEventListener('click', function() {
+  let randomNum = currentItem;
+  while( randomNum == currentItem) {
+    randomNum = Math.floor(Math.random() * reviews.length)
+  }
+  currentItem = randomNum;
+  showPerson(currentItem);
+})
