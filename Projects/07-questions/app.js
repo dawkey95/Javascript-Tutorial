11cadb13e6e1c83baf065b36b68c6c5171eb6076
dawidkeyser <dawidkeyser95@gmail.com>
2022-09-06 18:58:23 +1200

    Buttons JS added to show text

--- Projects/07-questions/app.js
+++ Projects/07-questions/app.js
@@ -1,2 +1,11 @@
 //using selectors inside the element
 // traversing the dom
+
+const questionBtns = document.querySelectorAll('.question-btn');
+
+questionBtns.forEach(function(btn) {
+  btn.addEventListener('click', function(e) {
+    const question = e.currentTarget.parentElement.parentElement;
+    question.classList.toggle('show-text');
+  });
+});
