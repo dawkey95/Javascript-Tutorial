b83a93302d83c333ec5f9fcbff4c736b6fb88872
dawidkeyser <dawidkeyser95@gmail.com>
2022-09-07 15:47:51 +1200

    Questions Project Completed

--- Projects/07-questions/app.js
+++ Projects/07-questions/app.js
@@ -1,11 +1,26 @@
 //using selectors inside the element
+const questions = document.querySelectorAll('.question');
+
+questions.forEach(function(question) {
+  // console.log(question);
+  const btn = question.querySelector('.question-btn');
+  btn.addEventListener('click', function() {
+    questions.forEach(function(item) {
+      if(item !== question) {
+        item.classList.remove('show-text');
+      }
+    })
+
+    question.classList.toggle('show-text')
+  })
+})
+
 // traversing the dom
+// const questionBtns = document.querySelectorAll('.question-btn');
 
-const questionBtns = document.querySelectorAll('.question-btn');
-
-questionBtns.forEach(function(btn) {
-  btn.addEventListener('click', function(e) {
-    const question = e.currentTarget.parentElement.parentElement;
-    question.classList.toggle('show-text');
-  });
-});
+// questionBtns.forEach(function(btn) {
+//   btn.addEventListener('click', function(e) {
+//     const question = e.currentTarget.parentElement.parentElement;
+//     question.classList.toggle('show-text');
+//   });
+// });
