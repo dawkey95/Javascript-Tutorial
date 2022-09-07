2b099d27ddc55609a7b6d0cd63b6ba48e785187a
dawidkeyser <dawidkeyser95@gmail.com>
2022-09-07 18:51:00 +1200

    JS started for Menu Project

--- Projects/08-menu/app.js
+++ Projects/08-menu/app.js
@@ -72,3 +72,21 @@ const menu = [
     desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
   },
 ];
+
+const sectionCenter = document.querySelector('.section-center');
+
+window.addEventListener('DOMContentLoaded', function() {
+  let displayMenu = menu.map(function(item) {
+    return `<article class="menu-item">
+          <img src="./menu-item.jpeg" class="photo" alt="menu-item">
+          <div class="item-info">
+            <header>
+              <h4>${item.title}</h4>
+            <h4 class="price">${item.price}</h4>
+            </header>
+            <p class="item-text">${item.desc}</p>
+          </div>
+        </article>`
+  })
+  console.log(displayMenu);
+})
