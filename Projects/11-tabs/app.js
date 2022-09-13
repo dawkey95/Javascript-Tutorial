23efb6ca5a6549ac544ca83134b86fd8d0926987
dawidkeyser <dawidkeyser95@gmail.com>
2022-09-14 08:25:30 +1200

    comment update

--- Projects/11-tabs/app.js
+++ Projects/11-tabs/app.js
@@ -5,16 +5,19 @@ const articles = document.querySelectorAll('.content');
 about.addEventListener('click', function (e) {
 	const id = e.target.dataset.id;
 	if (id) {
-		// remove active from all buttons
+		// remove 'active' from all buttons and add 'active' to targeted button
 		tabBtns.forEach(function (btn) {
 			btn.classList.remove('active');
 			e.target.classList.add('active');
 		});
 
-		// hide all articles and display matching id
+		// hide all articles
 		articles.forEach(function (article) {
 			article.classList.remove('active');
 		});
+
+    // create new element variable that get the 'id' from the dataset.id
+    // so each article with that dataset id will be displayed accordingly
 		const element = document.getElementById(id);
     element.classList.add('active');
 	}
