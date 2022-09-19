25326073b7df7f009e6f281217dca87d736320da
dawidkeyser <dawidkeyser95@gmail.com>
2022-09-20 07:33:00 +1200

    app JS list alerts

--- Projects/14-grocery-bud/app.js
+++ Projects/14-grocery-bud/app.js
@@ -13,32 +13,64 @@ let editElement;
 let editFlag = false;
 let editID = '';
 
-
 // ****** EVENT LISTENERS **********
 // submit form
-form.addEventListener('submit', addItem)
-
+form.addEventListener('submit', addItem);
 
 // ****** FUNCTIONS **********
 function addItem(e) {
-  e.preventDefault();
-  const value = grocery.value;
-  const id = new Date().getTime().toString();
+	e.preventDefault();
+	const value = grocery.value;
+	const id = new Date().getTime().toString();
+
+	if (value && !editFlag) {
+		const element = document.createElement('article');
+		// add class
+		element.classList.add('grocery-item');
+
+		// add ID
+		const attr = document.createAttribute('data-id');
+		attr.value = id;
+		element.setAttributeNode(attr);
+		element.innerHTML = `<p class="title">${value}</p>
+    <div class="btn-container">
+      <button type="button" class="edit-btn">
+        <i class="fas fa-edit"></i>
+      </button>
+      <button type="button" class="delete-btn">
+        <i class="fas fa-trash"></i>
+      </button>
+    </div>`;
+
+    // append child
+    list.appendChild(element);
+    // display alert
+    displayAlert('item added to the list', 'success')
+    // show container
+    groceryCont.classList.add('show-container');
+	} 
   
-  if(value !== '' && editFlag === false) {
-    console.log('add item to the list');
-  }
-  else if(value !== '' && editFlag === true) {
-    console.log('editing');
-  }
+  else if (value && editFlag) {
+		console.log('editing');
+	} 
+  
   else {
-    console.log('empty value');
-  }
+		displayAlert('please enter value', 'danger');
+	}
 }
 
+// display alert
+function displayAlert(text, action) {
+	alert.textContent = text;
+	alert.classList.add(`alert-${action}`);
+
+	// remove alert
+	setTimeout(function () {
+		alert.textContent = '';
+		alert.classList.remove(`alert-${action}`);
+	}, 2000);
+}
 
 // ****** LOCAL STORAGE **********
-
-
 
 // ****** SETUP ITEMS **********
